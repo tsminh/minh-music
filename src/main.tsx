be9f6b07b654f0db51223f956e43550483517abe
Minh To <hieuminh1198@gmail.com>
2026-06-13 15:09:18 +0700

    feat: add buffer

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,9 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import VConsole from "vconsole";
+import { Buffer } from "buffer";
+
+(globalThis as any).Buffer = Buffer;
 
 new VConsole();
 
